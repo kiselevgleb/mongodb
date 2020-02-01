cc69d3bead5db7772bc9f9f009e347420249e37c
kiselevgleb <kiselevgleb@gmail.com>
2020-02-01 15:52:02 +0300

    Initial commit

---
+++ src/main/java/com/mongo0102/mongo0102/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.mongo0102.mongo0102;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
---
+++ src/test/java/com/mongo0102/mongo0102/Mongo0102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo0102.mongo0102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mongo0102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
